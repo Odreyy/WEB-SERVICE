5dc6db246aadcd19246c9b6e36e898051f8df6cc
Odreyy <128235467+Odreyy@users.noreply.github.com>
2024-07-09 16:45:00 +0800

    Add files via upload

---
+++ app/Http/Middleware/PemilikPostingan.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class PemilikPostingan
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $currentUser = Auth::user();
+        $post = Post::findOrFail($request->id);
+
+        if($post->author != $currentUser->id) {
+            return response()->json(['message' => 'data not found']);
+        }
+
+        return $next($request);
+    }
+}
